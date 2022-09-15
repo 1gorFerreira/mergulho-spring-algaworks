d3f077b4e0e757939e6f24beed4d2116bb6de7b8
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-09-15 04:16:15 -0300

    Using Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -2,24 +2,28 @@ package com.algaworks.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	private ClienteRepository clienteRepository;
+	
+	//Outra forma de injetar dependencia além do uso do @Autowired
+	/*public ClienteController(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}*/
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar(){
-		return entityManager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findByNome("João");
 	}
 
 }
---
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.algaworks.algalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	
+}
