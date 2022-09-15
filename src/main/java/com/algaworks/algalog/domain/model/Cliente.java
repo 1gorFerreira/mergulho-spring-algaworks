7e1ac4b5a67d3df6cb4f799fdf70cbfce084f75a
Igor Ferreira <igorferreiradaniel99@hotmail.com>
2022-09-15 03:54:00 -0300

    Using jakarta Persistence

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,30 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 }
