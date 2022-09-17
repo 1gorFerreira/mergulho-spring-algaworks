39b4cdf4a82e7c06de55ff1f237b85a25752a30b
Igor <igorferreiradaniel99@hotmail.com>
2022-09-17 16:37:23 -0300

    Cascade validation and validation groups

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -7,7 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.validation.groups.Default;
+
+import com.algaworks.algalog.domain.ValidationGroups;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,12 +23,17 @@ import lombok.Setter;
 @Entity
 public class Cliente {
 
+	// Criamos um grupo de validação diferente do Default para dizer para outras validações externas que não é
+	// possível referenciar um cliente sem passar o Id, se não fizermos isso, ele ficará com o grupo Default
+	// e até os endpoints de clientes irá ser obrigátorio passar o id (O que não faz sentido, já que é autoincrementavel);
+	// PS: Os @Valid nos endpoints das classes controllers usam como padrão os groups = Default.class;
+	@NotNull(groups = ValidationGroups.ClienteId.class) 
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank
+	@NotBlank(groups = Default.class) // (groups = ...) Apenas para representar que todas as anotações de validações possuem esse grupo naturalmente;
 	@Size(max = 60)
 	private String nome;
 	
--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -12,7 +12,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -31,13 +36,19 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Valid // Faz as validações do objeto Cliente;
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)// Diz para validar o cliente no grupo ValidationGroups.ClienteId;
+	@NotNull //Faz com que não seja aceito um clinte vazio ao tentar gerar uma nova Entrega;
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	//Usaremos o READ_ONLY para evitar que o consumidor da API passe esses atributos na requisição;
